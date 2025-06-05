d0774005197d9655413a2fe313a6bc4ca318f361
seblaf1 <laflammesebastien@hotmail.com>
2025-06-05 17:36:48 -0400

    add portfolio_solver signature

--- Utils/portfolio_solver.py
+++ Utils/portfolio_solver.py
@@ -1,17 +1,15 @@
-﻿from mysql.connector.aio import MySQLConnectionAbstract
-from pandas import DataFrame
-
-from data.portfolios import Portfolio
+﻿from data.portfolios import Portfolio
+from data.stock_metadata import StockSnapshot
 from data.stocks import Stock
 
 
 class PortfolioSolver:
-    def __init__(self, stocks: dict[Stock, float], penalty_factor=0.00001, max_weight_threshold=0.3, risk_aversion=0.3):
+    def __init__(self, stocks: dict[Stock, StockSnapshot], penalty_factor=0.00001, max_weight_threshold=0.3, risk_aversion=0.3):
         self._stocks = stocks
         self.penalty_factor = penalty_factor
         self.max_weight_threshold = max_weight_threshold
         self.risk_aversion = risk_aversion
         
         
-    def solve(self, close_history: DataFrame) -> Portfolio:
-        return
+    def solve(self) -> Portfolio:
+
--- data/signals.py
+++
@@ -1,6 +0,0 @@
-﻿from data.stocks import Stock
-
-class StockSignal:
-    def __init__(self, stock: Stock, score: float):
-        self.stock = stock
-        self.score = score
