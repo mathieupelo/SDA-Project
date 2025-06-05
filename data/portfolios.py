5187cce216b42203d20231892e8e0a6884e49584
seblaf1 <laflammesebastien@hotmail.com>
2025-06-05 14:35:16 -0400

    add deprecated decorator to old methods

--- data/portfolios.py
+++ data/portfolios.py
@@ -5,18 +5,11 @@ from datetime import date
 
 class Portfolio:
     class PStock:
-        class PSignal:
-            def __init__(self, signal, version: int, score: float):
-                self.signal = signal
-                self.version = version
-                self.score = score
-
         # Portfolio.Stock
-        def __init__(self, stock: Stock, weight: float, alpha_score: float, signals: List[PSignal]):
+        def __init__(self, stock: Stock, weight: float, alpha_score: float):
             self.stock = stock
             self.weight = weight
             self.alpha_score = alpha_score
-            self.signals = signals
 
     # Portfolio
     def __init__(self, p_id: str, creation_date: date, stocks: List[PStock]):
@@ -25,10 +18,9 @@ class Portfolio:
         self.stocks = stocks
 
 
-
-
     # def get_signals(self) -> List[SignalBase]:
     # def get_weight_table(self) -> pd.DataFrame:
+
 
 def get_portfolio(conn: MySQLConnectionAbstract, portfolio_id: str) -> Portfolio | None:
     """
@@ -58,11 +50,11 @@ def get_portfolio(conn: MySQLConnectionAbstract, portfolio_id: str) -> Portfolio
                    WHERE ps.portfolio_id = %s
                    """, (portfolio_id,))
     rows = cursor.fetchall()
-
     stocks: List[Portfolio.PStock] = []
+
     for stock_id, name, ticker, weight, alpha_score in rows:
         stock = Stock(stock_id, name, ticker)
-        stocks.append(Portfolio.PStock(stock, weight, alpha_score, signals=[]))
+        stocks.append(Portfolio.PStock(stock, weight, alpha_score))
 
     return Portfolio(p_id=portfolio_id, creation_date=creation_date, stocks=stocks)
 
--- scripts/fetch_stock.py
+++ scripts/fetch_stock.py
@@ -3,9 +3,8 @@ import yfinance as yf
 from data.utils import connect_to_database
 
 def fetch_stocks(ticker_list, host: str):
-    rows = []
-    conn = connect_to_database(host)
 
+    rows = []
     for stock_symbol in ticker_list:
         stock = yf.Ticker(stock_symbol)
         stock_id = str(uuid.uuid4())
@@ -17,6 +16,7 @@ def fetch_stocks(ticker_list, host: str):
 
         rows.append((stock_id, stock_symbol, stock_name))
 
+    conn = connect_to_database(host)
     with conn.cursor() as cursor:
         sql = "INSERT IGNORE INTO stock (id, ticker, name) VALUES (%s, %s, %s)"
         cursor.executemany(sql, rows)
